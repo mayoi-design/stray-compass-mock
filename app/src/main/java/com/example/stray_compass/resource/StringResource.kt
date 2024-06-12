e4bb5164d574dd322b0f647c48be36c6ef4fedae
Yourein <rein.you.3.1@gmail.com>
2024-06-12 17:07:47 +0900

    定数を定義するファイルを作成

---
+++ app/src/main/java/com/example/stray_compass/resource/StringResource.kt
@@ -0,0 +1,5 @@
+package com.example.stray_compass.resource
+
+const val locationIntentAction = "STRAY_COMPASS_LOCATION_BROADCAST"
+const val locationIntentLatitude = "Latitude"
+const val locationIntentLongitude = "Longitude"
