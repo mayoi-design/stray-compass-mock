2c23ce6320da84c7c8857665602e1488e04c06ec
Yourein <rein.you.3.1@gmail.com>
2024-06-29 20:31:44 +0900

    DebugTextの出しわけをするPreferenceを設定

--- app/src/main/java/com/example/stray_compass/resource/StringResource.kt
+++ app/src/main/java/com/example/stray_compass/resource/StringResource.kt
@@ -1,5 +1,9 @@
 package com.example.stray_compass.resource
 
+import androidx.datastore.preferences.core.booleanPreferencesKey
+
 const val locationIntentAction = "STRAY_COMPASS_LOCATION_BROADCAST"
 const val locationIntentLatitude = "Latitude"
 const val locationIntentLongitude = "Longitude"
+
+val mainActivityDebugTextPreference = booleanPreferencesKey("MAINACTIVITY_DEBUG_TEXT_PREFERENCE")
