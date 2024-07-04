cd9de3dfc901ba2a94dc58de1ea0d609beefd200
Yourein <rein.you.3.1@gmail.com>
2024-07-04 18:15:31 +0900

    LongitudeとLatitudeが逆になっていた部分を修正

--- app/src/main/java/com/example/stray_compass/LocationExtension.kt
+++ app/src/main/java/com/example/stray_compass/LocationExtension.kt
@@ -2,7 +2,11 @@ package com.example.stray_compass
 
 import android.location.Location
 
-fun doubleToLocation(lat: Double, lng: Double): Location {
+/**
+ * Create a Location object from two Double values.
+ * lng = Longitude (緯度), lat = Latitude (経度).
+ */
+fun doubleToLocation(lng: Double, lat: Double): Location {
     val location = Location(null)
     location.latitude = lat
     location.longitude = lng
