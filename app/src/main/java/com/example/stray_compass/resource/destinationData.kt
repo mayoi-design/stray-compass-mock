cd1eb82b614b2b75303ebb8ddb83f2cf56f7d68f
Yourein <rein.you.3.1@gmail.com>
2024-07-04 18:27:16 +0900

    選択できる目的地の緯度経度が多少違っていたのを修正

--- app/src/main/java/com/example/stray_compass/resource/destinationData.kt
+++ app/src/main/java/com/example/stray_compass/resource/destinationData.kt
@@ -9,10 +9,11 @@ data class Destination(
 )
 
 val destinationList = listOf(
-    Destination("函館山", doubleToLocation(41.759167, 140.704444)),
-    Destination("立待岬", doubleToLocation(41.74564393712656, 140.72217385783742)),
-    Destination("新函館北斗駅", doubleToLocation(41.904975472356206, 140.64916583040576)),
-    Destination("新中野ダム", doubleToLocation(41.87060500660374, 140.78680017848293)),
-    Destination("北海道東照宮", doubleToLocation(41.839274713428516, 140.78669408732782)),
-    Destination("蔦屋書店", doubleToLocation(41.83622834602887, 140.7494532874035))
+    Destination("函館山", doubleToLocation(41.7603614, 140.7045022)),
+    Destination("立待岬", doubleToLocation(41.7452515, 140.7215674)),
+    Destination("函館熱帯植物園", doubleToLocation(41.7740046, 140.7895020)),
+    Destination("新函館北斗駅", doubleToLocation(41.9046990, 140.6483768)),
+    Destination("新中野ダム", doubleToLocation(41.8706050, 140.7868001)),
+    Destination("北海道東照宮", doubleToLocation(41.8386536, 140.7850566)),
+    Destination("蔦屋書店", doubleToLocation(41.8306748, 140.7356658))
 )
